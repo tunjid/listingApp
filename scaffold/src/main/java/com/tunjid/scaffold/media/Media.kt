55c966c945b07e7988a2c8784796acac109721f8
Adetunji Dahunsi <tjdah100@gmail.com>
2024-03-16 10:14:31 -0400

    Interpolat content scale when performing shared element transition

--- scaffold/src/main/java/com/tunjid/scaffold/media/Media.kt
+++ scaffold/src/main/java/com/tunjid/scaffold/media/Media.kt
@@ -1,17 +1,24 @@
 package com.tunjid.scaffold.media
 
 import android.webkit.MimeTypeMap
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.AndroidExternalSurface
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ScaleFactor
+import androidx.compose.ui.layout.lerp
 import androidx.compose.ui.platform.LocalContext
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
@@ -31,7 +38,7 @@ fun Media(
                 modifier = Modifier.fillMaxSize(),
                 model = url,
                 contentDescription = null,
-                contentScale = mediaArgs.contentScale
+                contentScale = mediaArgs.contentScale.interpolate()
             )
 
             mimeType.startsWith("video") -> VideoPlayer(
@@ -40,6 +47,90 @@ fun Media(
             )
         }
     }
+}
+
+@Composable
+private fun ContentScale.interpolate(): ContentScale {
+    var interpolation by remember {
+        mutableFloatStateOf(1f)
+    }
+    var previousScale by remember {
+        mutableStateOf(this)
+    }
+
+    val currentScale by remember {
+        mutableStateOf(this)
+    }.apply {
+        if (value != this@interpolate) previousScale = when {
+            interpolation == 1f -> value
+            else -> CapturedContentScale(
+                capturedInterpolation = interpolation,
+                previousScale = previousScale,
+                currentScale = value
+            )
+        }.also { interpolation = 0f }
+        value = this@interpolate
+    }
+
+    LaunchedEffect(currentScale) {
+        animate(
+            initialValue = 0f,
+            targetValue = 1f,
+            animationSpec = spring(
+                stiffness = Spring.StiffnessMediumLow
+            ),
+            block = { progress, _ ->
+                interpolation = progress
+            },
+        )
+    }
+
+    return remember {
+        object : ContentScale {
+            override fun computeScaleFactor(
+                srcSize: Size,
+                dstSize: Size
+            ): ScaleFactor {
+                val start = previousScale.computeScaleFactor(
+                    srcSize = srcSize,
+                    dstSize = dstSize
+                )
+                val stop = currentScale.computeScaleFactor(
+                    srcSize = srcSize,
+                    dstSize = dstSize
+                )
+
+                return if (start == stop) stop
+                else lerp(
+                    start = start,
+                    stop = stop,
+                    fraction = interpolation
+                )
+            }
+        }
+    }
+}
+
+private class CapturedContentScale(
+    private val capturedInterpolation: Float,
+    private val previousScale: ContentScale,
+    private val currentScale: ContentScale,
+
+    ) : ContentScale {
+    override fun computeScaleFactor(
+        srcSize: Size,
+        dstSize: Size
+    ): ScaleFactor = lerp(
+        start = previousScale.computeScaleFactor(
+            srcSize = srcSize,
+            dstSize = dstSize
+        ),
+        stop = currentScale.computeScaleFactor(
+            srcSize = srcSize,
+            dstSize = dstSize
+        ),
+        fraction = capturedInterpolation
+    )
 }
 
 fun String.mimeType(): String? {
