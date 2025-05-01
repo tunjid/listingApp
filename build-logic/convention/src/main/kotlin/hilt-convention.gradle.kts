77a46c0ad1883ac9119a23b4468c954609ae58db
Adetunji Dahunsi <tjdah100@gmail.com>
2025-05-01 19:40:58 -0400

    Remove references to kapt

--- build-logic/convention/src/main/kotlin/hilt-convention.gradle.kts
+++ build-logic/convention/src/main/kotlin/hilt-convention.gradle.kts
@@ -10,7 +10,4 @@ dependencies {
     "ksp"(libs.findLibrary("hilt.compiler").get())
     "kspAndroidTest"(libs.findLibrary("hilt.compiler").get())
     "kspTest"(libs.findLibrary("hilt.compiler").get())
-//    "kapt"(libs.findLibrary("hilt.compiler").get())
-//    "kaptAndroidTest"(libs.findLibrary("hilt.compiler").get())
-//    "kaptTest"(libs.findLibrary("hilt.compiler").get())
 }
