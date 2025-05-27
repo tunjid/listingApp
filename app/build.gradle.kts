55ab3a6923025fa594d281171a5284a8d8d8356e
Adetunji Dahunsi <tjdah100@gmail.com>
2025-05-27 16:31:43 -0400

    Flatten features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,9 +59,9 @@ dependencies {
     implementation(project(":scaffold"))
 
     implementation(project(":feature-favorites"))
-    implementation(project(":feature-listing:detail"))
-    implementation(project(":feature-listing:feed"))
-    implementation(project(":feature-listing:gallery"))
+    implementation(project(":feature-listing-detail"))
+    implementation(project(":feature-listing-feed"))
+    implementation(project(":feature-listing-gallery"))
     implementation(project(":feature-messages"))
     implementation(project(":feature-profile"))
     implementation(project(":feature-explore"))
